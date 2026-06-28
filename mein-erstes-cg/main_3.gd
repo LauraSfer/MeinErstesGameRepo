80f26ff1373c2207b9eddf6cafc63963dd8ad27b
LauraSfer <lauraseefelde333@gmail.com>
2026-06-28 18:01:50 +0200

    schießen

---
+++ mein-erstes-cg/main_3.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) :
+	if Input.is_action_just_pressed("ui_accept"): # Leertaste
+		print("shoooot")
+		$kugel.apply_impulse(Vector3(0, 0, -200))
+		
+	
+	pass
